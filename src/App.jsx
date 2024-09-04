a1aeb452432a0c4b8de8044742eab784ea4311be
masoudMarde <mardmasoud66@gmail.com>
2024-09-04 19:36:56 +0330

    v3

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,49 @@
-
-import { useEffect, useState } from 'react'
-import './App.css'
-import { useRef } from 'react'
+import { useEffect, useState } from "react";
+import "./App.css";
+import { useRef } from "react";
 
 // eslint-disable-next-line react/prop-types
-function Content({content,class1 ,el}){
-
-//  const el = useRef({})
-u
-  return (<div className={`content ${class1}` } ref={el} >content {content}</div>)
-  
-}
-
-
-function App() {
-//  window.onscroll=(function(){
-//   console.log()
-//  setShow(true)
-//  })
-const el = useRef()
- const contents=[1,2,3]
-const [show,setShow]=useState(false)
-
+function Content({ content }) {
+  //  const el = useRef({})
+  const [scr, setScr] = useState(false);
+  // window.onscroll = function () {
+  //   setScr(true);
+  //   console.log("scr");
+  // };
+  console.log("inner:", window.innerHeight);
+  const el = useRef(null);
+  const [show, setShow] = useState(false);
+  useEffect(function () {
+    if (el.current.getBoundingClientRect().bottom > window.innerHeight)
+      setShow(true);
+    console.log(el.current.getBoundingClientRect().bottom < window.innerHeight);
+  }, []);
 
   return (
-    <div className='container'  >
-    
-   {
-   contents.map(content=>( show&&content>5) ?<Content class1='x'  key={content} content={content}  el={el}  /> : <Content class1='y' key={content} content={content} el={el} />)}
+    <div
+      className={scr ? "x content" : show ? " content1" : " content"}
+      ref={el}
+    >
+      <p>
+        show: {Number(show)} scr:{Number(scr)}
+      </p>
     </div>
-  )
-  
+  );
 }
 
-export default App
+function App() {
+  const contents = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+  return (
+    <div className="container">
+      {contents.map((content) => (
+        <Content
+          key={content}
+          // content={content}
+        />
+      ))}
+    </div>
+  );
+}
+
+export default App;
