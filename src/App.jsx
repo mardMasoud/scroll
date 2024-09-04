7c2af5976180b2694b48013df9344a82106bd6f0
masoudMarde <mardmasoud66@gmail.com>
2024-09-05 01:59:37 +0330

    finally

--- src/App.jsx
+++ src/App.jsx
@@ -3,43 +3,59 @@ import "./App.css";
 import { useRef } from "react";
 
 // eslint-disable-next-line react/prop-types
-function Content({ content }) {
+function Content({ content, index, scr }) {
   //  const el = useRef({})
-  const [scr, setScr] = useState(false);
-  // window.onscroll = function () {
-  //   setScr(true);
-  //   console.log("scr");
-  // };
-  console.log("inner:", window.innerHeight);
+  // console.log("ind", 5 + index * 105);
+
+  // console.log("inner:", window.innerHeight);
   const el = useRef(null);
   const [show, setShow] = useState(false);
-  useEffect(function () {
-    if (el.current.getBoundingClientRect().bottom > window.innerHeight)
-      setShow(true);
-    console.log(el.current.getBoundingClientRect().bottom < window.innerHeight);
-  }, []);
+  useEffect(
+    function () {
+      if (el.current.getBoundingClientRect().top < window.innerHeight)
+        setShow(true);
+      if (el.current.getBoundingClientRect().bottom > window.innerHeight)
+        setShow(false);
+      // console.log(el.current.getBoundingClientRect().top < window.innerHeight);
+    },
+    [scr]
+  );
 
   return (
     <div
-      className={scr ? "x content" : show ? " content1" : " content"}
+      className={
+        window.innerHeight > 5 + index * 105
+          ? " content"
+          : scr && show
+          ? "y content"
+          : index % 2 === 0
+          ? "content1"
+          : "content2"
+      }
       ref={el}
     >
-      <p>
-        show: {Number(show)} scr:{Number(scr)}
-      </p>
+      <p>show: scr:{Number(scr)}</p>
     </div>
   );
 }
 
 function App() {
-  const contents = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
+  let scrolly = 0;
+  const contents = [
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+  ];
+  const [scr, setScr] = useState(0);
+  window.onscroll = function () {
+    setScr(window.scrollY);
+  };
   return (
     <div className="container">
       {contents.map((content) => (
         <Content
           key={content}
           // content={content}
+          index={content}
+          scr={scr}
         />
       ))}
     </div>
